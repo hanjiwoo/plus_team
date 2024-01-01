c7a8b43e4e750123628adedeefdb693adf9c1ba7
아름 <aaarem17@gmail.com>
2024-01-01 19:02:30 +0900

    반응속도게임 css 완료

--- src/components/ResponseCheck/ResponseCheck.jsx
+++ src/components/ResponseCheck/ResponseCheck.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useRef, useCallback, useMemo } from "react";
-import { Stscreen } from "./style";
+import { StContainer, Stscreen, StText, StTime } from "./style";
 
 const ResponseCheck = () => {
   const [state, setState] = useState("waiting");
-  const [message, setMessage] = useState("클릭해서 시작하세요.");
+  const [message, setMessage] = useState("클릭해서 시작하세요 !");
   const [result, setResult] = useState([]);
   const timeout = useRef(null);
   const startTime = useRef(0);
@@ -13,21 +13,24 @@ const ResponseCheck = () => {
     if (state === "waiting") {
       timeout.current = setTimeout(() => {
         setState("now");
-        setMessage("지금 클릭");
+        setMessage("지금 클릭 !!");
         startTime.current = new Date();
       }, Math.floor(Math.random() * 1000) + 2000); // 2초~3초 랜덤
       setState("ready");
-      setMessage("초록색이 되면 클릭하세요.");
+      setMessage("초록색이 되면 클릭하세요 !");
     } else if (state === "ready") {
       // 성급하게 클릭
       clearTimeout(timeout.current);
       setState("waiting");
-      setMessage("너무 성급해요! 초록색이 된 후에 클릭하세요!");
+      setMessage(`
+      너무 성급해요 ! 
+      초록색이 되면 클릭하세요 !
+    `);
     } else if (state === "now") {
       // 반응속도 체크
       endTime.current = new Date();
       setState("waiting");
-      setMessage("클릭해서 시작하세요.");
+      setMessage("클릭해서 시작하세요 !");
       setResult((prevResult) => {
         return [...prevResult, endTime.current - startTime.current];
       });
@@ -40,17 +43,24 @@ const ResponseCheck = () => {
   const renderAverage = () => {
     return result.length === 0 ? null : (
       <>
-        <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
-        <button onClick={onReset}>리셋</button>
+        <StTime>
+          {`반응한 속도의 평균은 ${
+            result.reduce((a, c) => a + c) / result.length
+          } ms 입니다 !`}
+          <button onClick={onReset}>리셋</button>
+        </StTime>
       </>
     );
   };
 
   return (
     <>
-      <Stscreen className={state} onClick={onClickScreen}>
-        {message}
-      </Stscreen>
+      <StContainer>
+        <Stscreen className={state} onClick={onClickScreen}>
+          <StText>{message}</StText>
+        </Stscreen>
+      </StContainer>
+
       {renderAverage()}
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import GameList from "../components/Home/GameList";
-import styled from "styled-components";
 
 export default function Home() {
   return (
