bee3f9b9c8bf2d4ec82a32600d4be000a3e400c1
SuinChoi <ctd2015@gmail.com>
2024-01-02 12:41:42 +0900

    최신소스 반영

--- src/components/ResponseCheck/ResponseCheck.jsx
+++ src/components/ResponseCheck/ResponseCheck.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useRef, useCallback } from "react";
-import { StContainer, Stscreen, StText, StTime } from "./style";
+import {
+  StContainer,
+  Stscreen,
+  StText,
+  StTime,
+  StContent,
+  StBtn,
+} from "./style";
 
 const ResponseCheck = () => {
   const [state, setState] = useState("waiting");
@@ -17,7 +24,7 @@ const ResponseCheck = () => {
         startTime.current = new Date();
       }, Math.floor(Math.random() * 1000) + 2000); // 2초~3초 랜덤
       setState("ready");
-      setMessage("초록색이 되면 클릭하세요 !");
+      setMessage("초록색이 되면 화면을 클릭하세요 !");
     } else if (state === "ready") {
       // 성급하게 클릭
       clearTimeout(timeout.current);
@@ -32,38 +39,40 @@ const ResponseCheck = () => {
       // 반응속도 체크
       endTime.current = new Date();
       setState("waiting");
-      setMessage("클릭해서 시작하세요 !");
+      setMessage("화면을 클릭하면 시작합니다.");
       setResult((prevResult) => {
         return [...prevResult, endTime.current - startTime.current];
       });
     }
   }, [state]);
+
+  // 평균 반응 속도 --> 초기화 버튼
   const onReset = useCallback(() => {
     setResult([]);
   }, []);
 
+  // 평균 반응 속도
   const renderAverage = () => {
     return result.length === 0 ? null : (
-      <>
+      <StContent>
         <StTime>
-          {`반응한 속도의 평균은 ${
+          {`평균 반응 속도 :  ${
             result.reduce((a, c) => a + c) / result.length
-          } ms 입니다 !`}
-          <button onClick={onReset}>리셋</button>
+          } ms`}
         </StTime>
-      </>
+        <StBtn onClick={onReset}>Reset</StBtn>
+      </StContent>
     );
   };
 
   return (
     <>
       <StContainer>
+        {renderAverage()}
         <Stscreen className={state} onClick={onClickScreen}>
           <StText>{message}</StText>
         </Stscreen>
       </StContainer>
-
-      {renderAverage()}
     </>
   );
 };
